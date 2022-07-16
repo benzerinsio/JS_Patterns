270ae875558ac8013752647a7893a8d44cddf565
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2022-07-16 15:28:32 -0300

    Creating a Module Design Pattern

--- section1/start/js/script.js
+++ section1/start/js/script.js
@@ -1,44 +1,50 @@
-var oneObjectNameSpace = oneObjectNameSpace || {};
-oneObjectNameSpace.JS_Pattern = oneObjectNameSpace.JS_Pattern || {};
-oneObjectNameSpace.JS_Pattern.benzerinsio = oneObjectNameSpace.JS_Pattern.benzerinsio || {};
-oneObjectNameSpace.JS_Pattern.benzerinsio.com = oneObjectNameSpace.JS_Pattern.benzerinsio.com || {};
-oneObjectNameSpace.JS_Pattern.benzerinsio.com.github = oneObjectNameSpace.JS_Pattern.benzerinsio.com.github || {};
+var chatModule = (function () {//module to set private and public access
 
-oneObjectNameSpace.JS_Pattern.benzerinsio.com.github = {
-	leadself: 'Me: ',
-	leadcomputer: "PC: ",
-	aSaid: ["This is a Cyber Chat"],
-	msgYes: "Yes, that's a great idea.",
-	msgNo: "No, that must be a mistake.",
-	aSassyStuff: ["Like mold on books, grow myths on history.",
-					"She moved like a poem and smiled like a sphinx.",
-					"As long as we don’t die, this is gonna be one hell of a story.",
-					"She laughed, and the desert sang.",
-					"You’ve got about as much charm as a dead slug."],
-	talk: function (msg) {
-		this.echo(this.leadself + msg);
-	},
-	replayYesNo: function() {
-		var msg = Math.random()>.5 ? this.msgYes : this.msgNo;
-		this.echo(this.leadcomputer + msg);
-	},
-	saySassyStuff: function() {
-		var msg = this.aSassyStuff[ Math.floor(Math.random()*this.aSassyStuff.length)];
-		this.echo(this.leadcomputer + msg);
-	},
-	echo: function(msg) {
-		this.aSaid.push("<div>" + msg + "</div>");
-
-		var aSaidLength = this.aSaid.length,
-			start = Math.max(aSaidLength - 6,0), 
-			out = "";
-
+	var leadself= 'Me: ',//private
+		leadcomputer= "PC: ",
+		aSaid= ["This is a Cyber Chat"],
+		msgYes= "Yes, that's a great idea.",
+		msgNo= "No, that must be a mistake.",
+		aSassyStuff= ["Like mold on books, grow myths on history.",
+				"She moved like a poem and smiled like a sphinx.",
+				"As long as we don’t die, this is gonna be one hell of a story.",
+				"She laughed, and the desert sang.",
+				"You’ve got about as much charm as a dead slug."];
+	
+	function echo(msg) {//private
+		aSaid.push("<div>" + msg + "</div>");
+			
+		var aSaidLength = aSaid.length,
+		start = Math.max(aSaidLength - 6,0), 
+				out = "";
+			
 		for(var i=start; i<aSaidLength; i++){
-			out += this.aSaid[i];
+			out += aSaid[i];
 		}
-
+			
 		$('.advert').html(out);
-
+			
 		$('#talk span').text(msg);
 	}
-};
+
+	return {//the public methods
+		talk: function (msg) {
+			echo(leadself + msg);
+		},
+		replyYesNo: function() {
+			var msg = Math.random()>.5 ? msgYes : msgNo;
+			echo(leadcomputer + msg);
+		},
+		saySassyStuff: function() {
+			var msg = aSassyStuff[ Math.floor(Math.random()*aSassyStuff.length)];
+			echo(leadcomputer + msg);
+		}
+
+	};
+})();
+
+$(document).ready(function(){
+	chatModule.talk("This is great");
+	chatModule.replyYesNo();
+	chatModule.saySassyStuff();
+});
